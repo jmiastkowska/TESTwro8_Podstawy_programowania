808b60c3e8f5ef06d9308fd18b5b10242e2bb7c1
aska <sasiek@o2.pl>
2020-07-12 12:50:59 +0200

    moj pierwszy program

---
+++ my-project/src/hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args){
+    System.out.println("witaj w programie");
+        String name = "Joanna";
+        String surname = "Miastkowska";
+        System.out.println(name + " " + surname);
+        System.out.println("koniec");
+    }
+}
